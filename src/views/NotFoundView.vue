e8b6d523ce6bebf5b95914c2b2c0eb7980b0f2d1
Eduarduar <eduardoarcega1997@gmail.com>
2024-08-22 10:23:39 -0600

    update: The page was updated not found

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="flex items-center justify-center h-screen bg-gray-100">
     <div class="text-center">
-      <h1 class="text-6xl font-bold text-gray-800">
+      <h1 class="text-9xl font-bold text-gray-800">
         <span class="text-emerald-500">4</span>0<span class="text-emerald-500">4</span>
       </h1>
-      <p class="text-xl text-gray-600">
+      <p class="text-4xl text-gray-600">
         <span class="text-emerald-500">O</span>ops! P<span class="text-emerald-500">á</span>gina
-        n<span class="text-emerald-500">o</span> enc<span class="text-emerald-500">0</span>ntr<span
+        n<span class="text-emerald-500">o</span> enc<span class="text-emerald-500">o</span>ntr<span
           class="text-emerald-500"
           >a</span
         >d<span class="text-emerald-500">a</span>.
       </p>
-      <a href="/" class="mt-4 text-blue-500 hover:underline">Volver a la página principal</a>
+      <a href="/" class="mt-4 text-blue-500 hover:underline text-2xl"
+        >Volver a la página principal</a
+      >
     </div>
   </div>
 </template>
