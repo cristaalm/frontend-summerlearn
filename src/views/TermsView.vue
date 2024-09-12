816d914d89f475d002be216b5af06ed967168379
gaboard <gabovalencia2009@gmail.com>
2024-09-12 15:01:05 -0600

    hotfix:Terms

--- src/views/TermsView.vue
+++ src/views/TermsView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-screen flex items-center justify-center bg-theme-2">
-      <div class="container max-w-4xl mx-auto p-4 bg-white shadow-md rounded-2xl flex flex-col">
+      <div class="container max-w-2xl mx-auto p-4 bg-white shadow-md rounded-2xl flex flex-col">
         <div class="text-2xl font-bold text-gray-800 mb-4 flex flex-row w-full justify-between">
             Términos y Condiciones
             <div>
