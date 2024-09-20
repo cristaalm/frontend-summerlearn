ab5d5e06dc3ee5acfbcea6243f2da7f192caa076
Eduarduar <eduardoarcega1997@gmail.com>
2024-09-20 16:07:11 -0600

    update: se actualizo useValidationAddArea para utilizar un patrón de expresión regular más conciso

--- src/hooks/donations/useDonations.js
+++ src/hooks/donations/useDonations.js
@@ -3,24 +3,22 @@ import { getDonations } from '@/services/donations/donations'
 import { useBarDonations } from './useBarDonations'
 
 export function useDonations() {
-    const donations = ref([])
-    const loading = ref(false)
-    const error = ref(null)
-    let barDonations = ref({})
+  const donations = ref([])
+  const loading = ref(false)
+  const error = ref(null)
+  let barDonations = ref({})
 
-    const loadDonations = async () => {
-        loading.value = true
-        try {
-            donations.value = await getDonations()
-            barDonations.value = useBarDonations(donations.value)
-        } catch (e) {
-            error.value = e
-        } finally {
-            loading.value = false
-        }
-
-        
+  const loadDonations = async () => {
+    loading.value = true
+    try {
+      donations.value = await getDonations()
+      barDonations.value = useBarDonations(donations.value)
+    } catch (e) {
+      error.value = e
+    } finally {
+      loading.value = false
     }
+  }
 
-    return { barDonations, donations, loading, error, loadDonations }
+  return { barDonations, donations, loading, error, loadDonations }
 }
--- src/services/donations/donations.js
+++ src/services/donations/donations.js
@@ -1,25 +1,21 @@
-import { Baseurl } from "@/../global";
+import { Baseurl } from '@/../global'
 
 export const getDonations = async () => {
+  const response = await fetch(`${Baseurl}donations/`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
 
-    const response = await fetch(`${Baseurl}donations/`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-        }
-    });
+  const json = await response.json()
+  const donations = json
 
-    const json = await response.json();
-    const donations = json;
-
-    return donations?.map(donation => ({
-        id: donation.donations_id,
-        concept: donation.donations_concept,
-        quanty: donation.donations_quantity,
-        date: donation.donations_date,
-        user: donation.donations_user,
-
-}));
-
-
+  return donations?.map((donation) => ({
+    id: donation.donations_id,
+    concept: donation.donations_concept,
+    quanty: donation.donations_quantity,
+    date: donation.donations_date,
+    user: donation.donations_user
+  }))
 }
