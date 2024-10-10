9b5d064a7f3b4dffd171e0d7bd66199497cbc665
Eduarduar <eduardoarcega1997@gmail.com>
2024-10-10 15:42:31 -0600

    Refactor AddArea and AddDonation components

--- src/views/dashboard/areas/AddArea.vue
+++ src/views/dashboard/areas/AddArea.vue
@@ -37,17 +37,14 @@ const handleRegister = () => {
       <div class="flex flex-col md:h-10 gap-y-3 md:items-center md:flex-row">
         <div class="text-base font-medium group-[.mode--light]:text-white">Agregar Área</div>
         <div class="flex flex-col sm:flex-row gap-x-3 gap-y-2 md:ml-auto">
-          <Button
-            variant="primary"
+          <Button variant="primary"
             class="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200 group-[.mode--light]:!border-transparent"
-            @click="
-              () => {
+            @click="() => {
                 router.push({
                   name: 'areas'
                 })
               }
-            "
-          >
+              ">
             <Lucide icon="ArrowLeft" class="stroke-[1.3] w-4 h-4 mr-2" /> Regresar
           </Button>
         </div>
@@ -55,45 +52,21 @@ const handleRegister = () => {
       <div class="mt-7">
         <div class="flex flex-col box box--stacked">
           <div class="p-7">
+
             <!--? ######################### ALERTA DE ERROR ######################### -->
 
-            <Alert
-              variant="outline-danger"
-              v-if="setAreaError"
-              class="flex items-center px-4 py-3 my-7 bg-danger/5 border-danger/20 rounded-[0.6rem] leading-[1.7]"
-              v-slot="{ dismiss }"
-            >
-              <div class="">
-                <Lucide
-                  icon="AlertTriangle"
-                  class="stroke-[0.8] stroke-danger w-7 h-7 mr-2 fill-danger/10"
-                />
-              </div>
-              <div class="ml-1 mr-8">
-                <span class="text-danger">{{ setAreaError }}</span>
-              </div>
-              <Alert.DismissButton
-                type="button"
-                class="btn-close text-danger"
-                @click="dismiss"
-                aria-label="Cerrar"
-              >
-                <Lucide icon="X" class="w-5 h-5" />
-              </Alert.DismissButton>
-            </Alert>
+            <Alert variant="outline-danger" v-if="setAreaError" :message="setAreaError" :dismissible="true"
+              class="flex items-center px-4 py-3 my-7" />
 
             <!--? ######################### INPUTS ######################### -->
 
-            <div
-              class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0"
-            >
+            <div class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0">
               <label class="inline-block mb-2 sm:mb-0 sm:mr-5 sm:text-right xl:w-60 xl:mr-14">
                 <div class="text-left">
                   <div class="flex items-center">
                     <div class="font-medium">Nombre del área</div>
                     <div
-                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200"
-                    >
+                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200">
                       Requerido
                     </div>
                   </div>
@@ -103,28 +76,20 @@ const handleRegister = () => {
                 </div>
               </label>
               <div class="flex-1 w-full mt-3 xl:mt-0">
-                <FormInput
-                  type="text"
-                  placeholder="Escriba aquí su nombre del área..."
-                  v-model="name"
-                  @input="(e) => validate(e, 'name')"
-                />
+                <FormInput type="text" placeholder="Escriba aquí su nombre del área..." v-model="name"
+                  @input="(e) => validate(e, 'name')" />
                 <div class="mt-1 text-xs text-red-500 h-4">
                   {{ status.name.message }}
                 </div>
               </div>
             </div>
 
-            <div
-              class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0"
-            >
+            <div class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0">
               <label class="inline-block mb-2 sm:mb-0 sm:mr-5 sm:text-right xl:w-60 xl:mr-14">
                 <div class="text-left">
                   <div class="flex items-center">
                     <div class="font-medium">Responsable</div>
-                    <div
-                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-xs rounded-md border border-slate-200"
-                    >
+                    <div class="ml-2.5 px-2 py-0.5 bg-slate-100 text-xs rounded-md border border-slate-200">
                       Requerido
                     </div>
                   </div>
@@ -163,15 +128,9 @@ const handleRegister = () => {
           <div class="flex py-5 border-t md:justify-end px-7 border-slate-200/80">
             <Button
               :class="`w-full px-10 md:w-auto font-bold ${setAreaLoading || !valid ? 'border-gray-500 text-gray-500' : 'border-green text-green'}`"
-              @click="handleRegister"
-              :disabled="!valid || setAreaLoading"
-            >
-              <LoadingIcon
-                v-if="setAreaLoading"
-                icon="tail-spin"
-                class="stroke-[1.3] w-4 h-4 mr-2 -ml-2"
-                color="black"
-              />
+              @click="handleRegister" :disabled="!valid || setAreaLoading">
+              <LoadingIcon v-if="setAreaLoading" icon="tail-spin" class="stroke-[1.3] w-4 h-4 mr-2 -ml-2"
+                color="black" />
 
               <Lucide v-if="!setAreaLoading" icon="Check" class="stroke-[1.3] w-4 h-4 mr-2" />
               {{ setAreaLoading ? 'Registrando...' : 'Registrar' }}
--- src/views/dashboard/programs/AddProgram.vue
+++ src/views/dashboard/programs/AddProgram.vue
@@ -52,17 +52,14 @@ const handleRegister = () => {
       <div class="flex flex-col md:h-10 gap-y-3 md:items-center md:flex-row">
         <div class="text-base font-medium group-[.mode--light]:text-white">Agregar Programa</div>
         <div class="flex flex-col sm:flex-row gap-x-3 gap-y-2 md:ml-auto">
-          <Button
-            variant="primary"
+          <Button variant="primary"
             class="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200 group-[.mode--light]:!border-transparent"
-            @click="
-              () => {
+            @click="() => {
                 router.push({
                   name: 'programs'
                 })
               }
-            "
-          >
+              ">
             <Lucide icon="ArrowLeft" class="stroke-[1.3] w-4 h-4 mr-2" /> Regresar
           </Button>
         </div>
@@ -70,45 +67,22 @@ const handleRegister = () => {
       <div class="mt-7">
         <div class="flex flex-col box box--stacked">
           <div class="p-7">
+
+
             <!--? ######################### ALERTA DE ERROR ######################### -->
 
-            <Alert
-              variant="outline-danger"
-              v-if="setProgramError"
-              class="flex items-center px-4 py-3 my-7 bg-danger/5 border-danger/20 rounded-[0.6rem] leading-[1.7]"
-              v-slot="{ dismiss }"
-            >
-              <div class="">
-                <Lucide
-                  icon="AlertTriangle"
-                  class="stroke-[0.8] stroke-danger w-7 h-7 mr-2 fill-danger/10"
-                />
-              </div>
-              <div class="ml-1 mr-8">
-                <span class="text-danger">{{ setProgramError }}</span>
-              </div>
-              <Alert.DismissButton
-                type="button"
-                class="btn-close text-danger"
-                @click="dismiss"
-                aria-label="Cerrar"
-              >
-                <Lucide icon="X" class="w-5 h-5" />
-              </Alert.DismissButton>
-            </Alert>
+            <Alert variant="outline-danger" v-if="setProgramError" :message="setProgramError" :dismissible="true"
+              class="flex items-center px-4 py-3 my-7" />
 
             <!--? ######################### INPUTS ######################### -->
 
-            <div
-              class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0"
-            >
+            <div class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0">
               <label class="inline-block mb-2 sm:mb-0 sm:mr-5 sm:text-right xl:w-60 xl:mr-14">
                 <div class="text-left">
                   <div class="flex items-center">
                     <div class="font-medium">Nombre del programa</div>
                     <div
-                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200"
-                    >
+                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200">
                       Requerido
                     </div>
                   </div>
@@ -118,27 +92,20 @@ const handleRegister = () => {
                 </div>
               </label>
               <div class="flex-1 w-full mt-3 xl:mt-0">
-                <FormInput
-                  type="text"
-                  placeholder="Escriba aqui el nombre del programa..."
-                  v-model="name"
-                />
+                <FormInput type="text" placeholder="Escriba aqui el nombre del programa..." v-model="name" />
                 <div class="mt-1 text-xs text-red-500 h-4">
                   {{ status.name.message }}
                 </div>
               </div>
             </div>
 
-            <div
-              class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0"
-            >
+            <div class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0">
               <label class="inline-block mb-2 sm:mb-0 sm:mr-5 sm:text-right xl:w-60 xl:mr-14">
                 <div class="text-left">
                   <div class="flex items-center">
                     <div class="font-medium">Escolaridad</div>
                     <div
-                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200"
-                    >
+                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200">
                       Requerido
                     </div>
                   </div>
@@ -173,16 +140,13 @@ const handleRegister = () => {
               </div>
             </div>
 
-            <div
-              class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0"
-            >
+            <div class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0">
               <label class="inline-block mb-2 sm:mb-0 sm:mr-5 sm:text-right xl:w-60 xl:mr-14">
                 <div class="text-left">
                   <div class="flex items-center">
                     <div class="font-medium text-nowrap">Duración del programa</div>
                     <div
-                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200"
-                    >
+                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200">
                       Requerido
                     </div>
                   </div>
@@ -192,40 +156,33 @@ const handleRegister = () => {
                 </div>
               </label>
               <div class="flex-1 w-full mt-3 xl:mt-0">
-                <Litepicker
-                  v-model="duration"
-                  :options="{
-                    autoApply: false,
-                    singleMode: false,
-                    numberOfColumns: 2,
-                    numberOfMonths: 2,
-                    showWeekNumbers: true,
-                    dropdowns: {
-                      minYear: new Date().getFullYear(),
-                      maxYear: new Date().getFullYear() + 1,
-                      months: true,
-                      years: true
-                    },
-                    format: 'DD/MM/YYYY'
-                  }"
-                  class="block w-full mx-auto"
-                />
+                <Litepicker v-model="duration" :options="{
+                  autoApply: false,
+                  singleMode: false,
+                  numberOfColumns: 2,
+                  numberOfMonths: 2,
+                  showWeekNumbers: true,
+                  dropdowns: {
+                    minYear: new Date().getFullYear(),
+                    maxYear: new Date().getFullYear() + 1,
+                    months: true,
+                    years: true
+                  },
+                  format: 'DD/MM/YYYY'
+                }" class="block w-full mx-auto" />
                 <div class="text-red-500 mt-2">
                   {{ status.duration.message }}
                 </div>
               </div>
             </div>
 
-            <div
-              class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0"
-            >
+            <div class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0">
               <label class="inline-block mb-2 sm:mb-0 sm:mr-5 sm:text-right xl:w-60 xl:mr-14">
                 <div class="text-left">
                   <div class="flex items-center">
                     <div class="font-medium">Responsable</div>
                     <div
-                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200"
-                    >
+                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200">
                       Requerido
                     </div>
                   </div>
@@ -260,16 +217,13 @@ const handleRegister = () => {
               </div>
             </div>
 
-            <div
-              class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0"
-            >
+            <div class="flex-col block pt-5 mt-5 xl:items-center sm:flex xl:flex-row first:mt-0 first:pt-0">
               <label class="inline-block mb-2 sm:mb-0 sm:mr-5 sm:text-right xl:w-60 xl:mr-14">
                 <div class="text-left">
                   <div class="flex items-center">
                     <div class="font-medium">Área</div>
                     <div
-                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200"
-                    >
+                      class="ml-2.5 px-2 py-0.5 bg-slate-100 text-slate-500 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md border border-slate-200">
                       Requerido
                     </div>
                   </div>
@@ -310,15 +264,9 @@ const handleRegister = () => {
           <div class="flex py-5 border-t md:justify-end px-7 border-slate-200/80">
             <Button
               :class="`w-full px-10 md:w-auto font-bold ${setProgramLoading || !valid ? 'border-gray-500 text-gray-500' : 'border-green text-green'}`"
-              @click="handleRegister"
-              :disabled="!valid || setProgramLoading"
-            >
-              <LoadingIcon
-                v-if="setProgramLoading"
-                icon="tail-spin"
-                class="stroke-[1.3] w-4 h-4 mr-2 -ml-2"
-                color="black"
-              />
+              @click="handleRegister" :disabled="!valid || setProgramLoading">
+              <LoadingIcon v-if="setProgramLoading" icon="tail-spin" class="stroke-[1.3] w-4 h-4 mr-2 -ml-2"
+                color="black" />
 
               <Lucide v-if="!setProgramLoading" icon="Check" class="stroke-[1.3] w-4 h-4 mr-2" />
               {{ setProgramLoading ? 'Registrando...' : 'Registrar' }}
