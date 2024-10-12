01faf66036b0beb8c9167e94206015739795041c
Eduarduar <eduardoarcega1997@gmail.com>
2024-10-11 18:33:27 -0600

    Refactor import paths and remove unused code in router/index.js

--- src/services/actividades/actividades.js
+++ src/services/actividades/actividades.js
@@ -1,4 +1,4 @@
-import { Baseurl } from '@/../global'
+import { Baseurl } from '@/utils/global'
 
 export const getActividades = async () => {
   const response = await fetch(`${Baseurl}activities/`, {
