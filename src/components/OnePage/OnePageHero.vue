0ae92abbb52a85fa88216eb586009c3eae7f3c2b
miguel huitron <mhuitron0@ucol.mx>
2024-10-20 18:24:08 -0600

    cambio  home

--- src/components/OnePage/OnePageHero.vue
+++ src/components/OnePage/OnePageHero.vue
@@ -12,6 +12,64 @@ const smoothScroll = (event) => {
 }
 </script>
 
+
+<template>
+  <section id="home"
+    className="relative min-h-screen h-fit w-full py-6 md:py-8 lg:py-10 flex flex-col justify-center items-center bg-esmerald-100/50 bg-transparent">
+    <div className="container px-4 md:px-6">
+      <div class="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+        <div
+          class="flex flex-col justify-center space-y-12"
+          data-aos="fade-right"
+          data-aos-delay="100"
+        >
+          <div class="space-y-6">
+            <h1 class="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+              {{ $t('hero.title') }}
+            </h1>
+            <p class="max-w-[600px] text-muted-foreground md:text-xl text-justify">
+              {{ $t('hero.description') }}
+            </p>
+          </div>
+          <div class="flex gap-2 flex-row justify-center md:justify-start">
+            <a
+              href="#joinOurTeam"
+              @click="smoothScroll"
+              class="inline-flex h-10 w-3/4 md:w-fit items-center justify-center cursor-pointer select-none rounded-md bg-emerald-500 px-8 py-6 text-xl font-medium text-white shadow transition-colors hover:bg-emerald-500/80 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 duration-300"
+            >
+              {{ $t('hero.button') }}
+            </a>
+          </div>
+        </div>
+        <img
+          src="/src/assets/images/Photo4.png"
+          alt="Hero"
+          class="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square w-[550] h-[550] z-10"
+          data-aos="fade-left"
+          data-aos-delay="100"
+        />
+      </div>
+
+    </div>
+    <!-- Zigzag separator -->
+    <div className="absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-[0]">
+      <svg className="relative block w-full h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <defs>
+          <linearGradient id="redGradientm" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" style="stop-color:#67de96;stop-opacity:1" />
+            <stop offset="100%" style="stop-color:#1aca60;stop-opacity:1" />
+          </linearGradient>
+        </defs>
+        <path fill="url(#redGradientm)" fill-opacity="1"
+          d="M0,160L80,186.7C160,213,320,267,480,277.3C640,288,800,256,960,218.7C1120,181,1280,139,1360,117.3L1440,96V320H0Z">
+        </path>
+      </svg>
+    </div>
+
+  </section>
+</template>
+
+<!-- 
 <template>
   <section
     id="home"
@@ -52,4 +110,4 @@ const smoothScroll = (event) => {
       </div>
     </div>
   </section>
-</template>
+</template> -->
--- src/components/OnePage/OnePageMission.vue
+++ src/components/OnePage/OnePageMission.vue
@@ -1,5 +1,72 @@
 <template>
   <section id="mission"
+    className="relative min-h-screen h-fit w-full py-6 md:py-8 lg:py-10 flex flex-col justify-center items-center bg-esmerald-100/50 bg-gradient-to-r from-hgeen1 to-hgeen2">
+    <div className="container px-4 md:px-6">
+      <div className="flex flex-col items-center justify-center space-y-4 text-center">
+        <div className="space-y-2" data-aos="fade-up" data-aos-delay="100">
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl ">
+            {{ $t('mission.title') }}
+          </h2>
+          <p
+            className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ">
+            {{ $t('mission.description') }}
+          </p>
+        </div>
+      </div>
+      <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12 text-justify">
+        <img src="/src/assets/images/Photo5.png" alt="Mission"
+          className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
+          width="550" height="310" data-aos="fade-left" data-aos-delay="100" />
+        <div className="flex flex-col justify-center space-y-4">
+          <ul className="grid gap-6">
+            <li>
+              <div className="grid gap-1" data-aos="fade-right" data-aos-delay="100">
+                <h3 className="text-xl font-bold">{{ $t('mission.values.1.title') }}</h3>
+                <p className="text-muted-foreground">
+                  {{ $t('mission.values.1.description') }}
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="grid gap-1" data-aos="fade-right" data-aos-delay="100">
+                <h3 className="text-xl font-bold">{{ $t('mission.values.2.title') }}</h3>
+                <p className="text-muted-foreground">
+                  {{ $t('mission.values.2.description') }}
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="grid gap-1" data-aos="fade-right" data-aos-delay="100">
+                <h3 className="text-xl font-bold">{{ $t('mission.values.3.title') }}</h3>
+                <p className="text-muted-foreground">
+                  {{ $t('mission.values.3.description') }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Zigzag separator -->
+    <div className="absolute bottom-0 left-0 right-0 w-full overflow-hidden leading-[0]">
+  <svg className="relative block w-full h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <defs>
+      <linearGradient id="gradient6" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style="stop-color:#f0e035;stop-opacity:1" />
+        <stop offset="100%" style="stop-color:#f88e15;stop-opacity:1" />
+      </linearGradient>
+    </defs>
+    <path fill="url(#gradient6)" fill-opacity="1"
+      d="M0,160L120,192C240,224,480,288,720,282.7C960,277,1200,203,1320,165.3L1440,128V320H0Z">
+    </path>
+  </svg>
+</div>
+  </section>
+</template>
+
+
+<!-- <template>
+  <section id="mission"
     className="min-h-screen w-full py-12 md:py-24 lg:py-32 flex flex-col justify-center items-center bg-gradient-to-r from-hgeen1 to-hgeen2 h-fit">
     <div className="container px-4 md:px-6">
       <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -48,4 +115,4 @@
       </div>
     </div>
   </section>
-</template>
+</template> -->
