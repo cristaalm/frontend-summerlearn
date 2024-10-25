689cc7ba32a3c9157219be0b5ff0caa61d47aa98
Eduarduar <eduardoarcega1997@gmail.com>
2024-10-24 23:23:17 -0600

    Refactor useFilter hooks to include additional search criteria

--- src/hooks/donations/useSearchDonations.js
+++ src/hooks/donations/useSearchDonations.js
@@ -1,12 +1,27 @@
 import { ref, computed } from 'vue'
 
+function formatDateToDDMMYYYY(dateString) {
+  if (!dateString) return '' // Verifica si dateString es undefined, null o vacío.
+  const [year, month, day] = dateString.split('-')
+  return `${day}/${month}/${year}`
+}
+
 export function useDonationSearch(donations) {
   const searchQuery = ref('') // Término de búsqueda para el concepto
 
   // Computed que filtra las donaciones en base al concepto, estado y categoría
   const filteredItems = computed(() => {
     return donations.value.filter((donation) => {
-      const matchesQuery = donation.concept.toLowerCase().includes(searchQuery.value.toLowerCase())
+      const date = formatDateToDDMMYYYY(donation.date)
+      const quenty = new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(
+        donation.quanty
+      )
+      const matchesQuery =
+        donation.concept.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+        quenty.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+        donation.user.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+        date.toLowerCase().includes(searchQuery.value.toLowerCase())
+
       return matchesQuery
     })
   })
