61a9c3927e36069509d83fb14af50d7216fd4cbb
Eduarduar <eduardoarcega1997@gmail.com>
2024-10-25 08:18:03 -0600

    Refactor delete area functionality and dialogs

---
+++ src/hooks/areas/Dialogs/useDialogDeteleArea.js
@@ -0,0 +1,15 @@
+export function useDialogDeleteArea() {
+  const ModalDeleteArea = ref(false)
+  const areaInfoProvideDelete = ref(null)
+  const setModalDeleteArea = ({ open, areaInfo = null }) => {
+    if (open) {
+      if (!areaInfoProvideDelete) return
+      ModalDeleteArea.value = open
+      areaInfoProvideDelete.value = areaInfo
+    } else {
+      ModalDeleteArea.value = open
+    }
+  }
+
+  return { ModalDeleteArea, setModalDeleteArea, areaInfoProvideDelete }
+}
