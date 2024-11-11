ca5ee91a37d97e1df676a736245d842643c9942d
Eduarduar <eduardoarcega1997@gmail.com>
2024-11-11 00:20:53 -0600

    fix: refactor child rendering logic for improved readability

--- src/views/dashboard/children/ChildrenViewBen.vue
+++ src/views/dashboard/children/ChildrenViewBen.vue
@@ -49,56 +49,59 @@ onMounted(() => {
             <div class="grid grid-cols-12 px-5 gap-5">
 
               <!-- ? Cuando se termina de cargar los hijos, se muestra el contenido -->
-              <template v-if="!loadingChildrens && !errorChildrens" v-for="(child, index) in childrens" :key="child.id">
-                <div class="col-span-12 sm:col-span-6 xl:col-span-4 px-5 py-5 flex flex-col box box--stacked">
-                  <div
-                    class="overflow-hidden rounded-lg h-52 image-fit before:block before:absolute before:w-full before:h-full before:top-0 before:left-0 before:z-10 before:bg-gradient-to-t before:from-slate-900/90 before:to-black/20">
-                    <img alt="Tailwise - Admin Dashboard Template" class="rounded-md select-none"
-                      :src="`${Baseurl}${child.photo}`" />
-                    <div class="absolute bottom-0 z-10 w-full px-5 pb-6 text-white">
-                      <span class="block text-lg font-medium truncate">
-                        {{ child.name }}
-                      </span>
-                      <span class="mt-3 text-xs text-white/80">
-                        {{ child.curp }}
-                      </span>
+              <template v-if="!loadingChildrens && !errorChildrens">
+
+                <template v-for="(child, index) in childrens" :key="child.id">
+                  <div class="col-span-12 sm:col-span-6 xl:col-span-4 px-5 py-5 flex flex-col box box--stacked">
+                    <div
+                      class="overflow-hidden rounded-lg h-52 image-fit before:block before:absolute before:w-full before:h-full before:top-0 before:left-0 before:z-10 before:bg-gradient-to-t before:from-slate-900/90 before:to-black/20">
+                      <img alt="Tailwise - Admin Dashboard Template" class="rounded-md select-none"
+                        :src="`${Baseurl}${child.photo}`" />
+                      <div class="absolute bottom-0 z-10 w-full px-5 pb-6 text-white">
+                        <span class="block text-lg font-medium truncate">
+                          {{ child.name }}
+                        </span>
+                        <span class="mt-3 text-xs text-white/80">
+                          {{ child.curp }}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                  <div class="pt-5">
-                    <div class="flex flex-col gap-3.5 mb-5 pb-5 mt-auto border-b border-dashed border-slate-300/70">
-                      <div class="flex items-center">
-                        <div class="text-slate-500 dark:text-slate-200">Fecha de nacimineto:</div>
-                        <div class="ml-auto">
-                          <div
-                            class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px">
-                            <span class="-mt-px">
-                              {{ child.birthdate }}
-                            </span>
+                    <div class="pt-5">
+                      <div class="flex flex-col gap-3.5 mb-5 pb-5 mt-auto border-b border-dashed border-slate-300/70">
+                        <div class="flex items-center">
+                          <div class="text-slate-500 dark:text-slate-200">Fecha de nacimineto:</div>
+                          <div class="ml-auto">
+                            <div
+                              class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px">
+                              <span class="-mt-px">
+                                {{ child.birthdate }}
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="flex items-center">
+                          <div class="text-slate-500 dark:text-slate-200">Edad:</div>
+                          <div class="ml-auto">
+                            <div
+                              class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px">
+                              <span class="-mt-px">
+                                {{ calculateAge(child.birthdate) }}
+                              </span>
+                            </div>
                           </div>
                         </div>
                       </div>
-                      <div class="flex items-center">
-                        <div class="text-slate-500 dark:text-slate-200">Edad:</div>
-                        <div class="ml-auto">
-                          <div
-                            class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px">
-                            <span class="-mt-px">
-                              {{ calculateAge(child.birthdate) }}
-                            </span>
-                          </div>
-                        </div>
+                      <div class="flex items-center justify-end">
+                        <Button class="flex items-center mr-3" variant="success">
+                          <Lucide icon="CheckSquare" class="w-4 h-4 stroke-[1.3] text-white" />
+                        </Button>
+                        <Button class="flex items-center text-danger" variant="danger">
+                          <Lucide icon="Trash2" class="w-4 h-4 stroke-[1.3] text-white" />
+                        </Button>
                       </div>
                     </div>
-                    <div class="flex items-center justify-end">
-                      <Button class="flex items-center mr-3" variant="success">
-                        <Lucide icon="CheckSquare" class="w-4 h-4 stroke-[1.3] text-white" />
-                      </Button>
-                      <Button class="flex items-center text-danger" variant="danger">
-                        <Lucide icon="Trash2" class="w-4 h-4 stroke-[1.3] text-white" />
-                      </Button>
-                    </div>
                   </div>
-                </div>
+                </template>
               </template>
 
               <!-- ? Cuando se está cargando los hijos, se muestra el icono de carga -->
