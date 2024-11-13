9a57c4b74d2840786b330c037157cc71f01fb2e0
Eduarduar <eduardoarcega1997@gmail.com>
2024-11-13 16:53:26 -0600

    feat: mejorar la estructura del componente ChildrenViewBen y optimizar el manejo de modales

--- src/views/dashboard/children/ChildrenViewBen.vue
+++ src/views/dashboard/children/ChildrenViewBen.vue
@@ -19,28 +19,40 @@ const { ModalEditChild, childInfoProvideEdit, setModalEditChild } = useDialogEdi
 onMounted(() => {
   loadChildrens()
 })
-
 </script>
 
 <template>
+  <DeleteChildModal
+    :ModalDeleteChild="ModalDeleteChild"
+    :setModalDeleteChild="setModalDeleteChild"
+    :infoChild="childInfoProvideDelete"
+  />
 
-  <DeleteChildModal :ModalDeleteChild="ModalDeleteChild" :setModalDeleteChild="setModalDeleteChild"
-    :infoChild="childInfoProvideDelete" />
-
-  <EditChildModal :ModalEditChild="ModalEditChild" :setModalEditChild="setModalEditChild"
-    :infoChild="childInfoProvideEdit" />
+  <EditChildModal
+    :ModalEditChild="ModalEditChild"
+    :setModalEditChild="setModalEditChild"
+    :infoChild="childInfoProvideEdit"
+  />
 
   <div class="grid grid-cols-12 gap-y-10 gap-x-6">
     <div class="col-span-12">
       <div class="flex flex-col md:h-10 gap-y-3 md:items-center md:flex-row">
-        <div class="text-base font-medium group-[.mode--light]:text-white" @click="() => {
-          errorChildrens = true
-        }">
+        <div
+          class="text-base font-medium group-[.mode--light]:text-white"
+          @click="
+            () => {
+              errorChildrens = true
+            }
+          "
+        >
           Mis Hijos
         </div>
         <div class="flex flex-col sm:flex-row gap-x-3 gap-y-2 md:ml-auto">
-          <Button variant="primary" @click="router.push({ name: 'addChildrens' })" class="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200
-            group-[.mode--light]:!border-transparent">
+          <Button
+            variant="primary"
+            @click="router.push({ name: 'addChildrens' })"
+            class="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200 group-[.mode--light]:!border-transparent"
+          >
             <Lucide icon="PenLine" class="stroke-[1.3] w-4 h-4 mr-2" />
             Agregar hijo
           </Button>
@@ -50,16 +62,20 @@ onMounted(() => {
         <div class="flex flex-col">
           <div class="overflow-hidden">
             <div class="grid grid-cols-12 px-5 gap-5">
-
               <!-- ? Cuando se termina de cargar los hijos, se muestra el contenido -->
               <template v-if="!loadingChildrens && !errorChildrens">
-
                 <template v-for="(child, index) in childrens" :key="child.id">
-                  <div class="col-span-12 sm:col-span-6 xl:col-span-4 px-5 py-5 flex flex-col box box--stacked">
+                  <div
+                    class="col-span-12 sm:col-span-6 xl:col-span-4 px-5 py-5 flex flex-col box box--stacked"
+                  >
                     <div
-                      class="overflow-hidden rounded-lg h-52 image-fit before:block before:absolute before:w-full before:h-full before:top-0 before:left-0 before:z-10 before:bg-gradient-to-t before:from-slate-900/90 before:to-black/20">
-                      <img alt="Tailwise - Admin Dashboard Template" class="rounded-md select-none"
-                        :src="`${Baseurl}${child.photo}`" />
+                      class="overflow-hidden rounded-lg h-52 image-fit before:block before:absolute before:w-full before:h-full before:top-0 before:left-0 before:z-10 before:bg-gradient-to-t before:from-slate-900/90 before:to-black/20"
+                    >
+                      <img
+                        alt="Tailwise - Admin Dashboard Template"
+                        class="rounded-md select-none"
+                        :src="`${Baseurl}${child.photo}`"
+                      />
                       <div class="absolute bottom-0 z-10 w-full px-5 pb-6 text-white">
                         <span class="block text-lg font-medium truncate">
                           {{ child.name }}
@@ -70,12 +86,15 @@ onMounted(() => {
                       </div>
                     </div>
                     <div class="pt-5">
-                      <div class="flex flex-col gap-3.5 mb-5 pb-5 mt-auto border-b border-dashed border-slate-300/70">
+                      <div
+                        class="flex flex-col gap-3.5 mb-5 pb-5 mt-auto border-b border-dashed border-slate-300/70"
+                      >
                         <div class="flex items-center">
                           <div class="text-slate-500 dark:text-slate-200">Fecha de nacimiento:</div>
                           <div class="ml-auto">
                             <div
-                              class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px">
+                              class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px"
+                            >
                               <span class="-mt-px">
                                 {{ child.birthdate }}
                               </span>
@@ -86,7 +105,8 @@ onMounted(() => {
                           <div class="text-slate-500 dark:text-slate-200">Edad:</div>
                           <div class="ml-auto">
                             <div
-                              class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px">
+                              class="flex items-center text-xs font-medium rounded-md text-success dark:text-slate-400 bg-success/10 border border-success/10 px-1.5 py-px"
+                            >
                               <span class="-mt-px">
                                 {{ calculateAge(child.birthdate) }}
                               </span>
@@ -95,14 +115,26 @@ onMounted(() => {
                         </div>
                       </div>
                       <div class="flex items-center justify-end">
-                        <Button class="flex items-center mr-3" variant="success" @click="() => {
-                          setModalEditChild({ open: true, childInfo: child })
-                        }">
+                        <Button
+                          class="flex items-center mr-3"
+                          variant="warning"
+                          @click="
+                            () => {
+                              setModalEditChild({ open: true, childInfo: child })
+                            }
+                          "
+                        >
                           <Lucide icon="CheckSquare" class="w-4 h-4 stroke-[1.3] text-white" />
                         </Button>
-                        <Button class="flex items-center text-danger" variant="danger" @click="() => {
-                          setModalDeleteChild({ open: true, childInfo: child })
-                        }">
+                        <Button
+                          class="flex items-center text-danger"
+                          variant="danger"
+                          @click="
+                            () => {
+                              setModalDeleteChild({ open: true, childInfo: child })
+                            }
+                          "
+                        >
                           <Lucide icon="Trash2" class="w-4 h-4 stroke-[1.3] text-white" />
                         </Button>
                       </div>
@@ -126,7 +158,6 @@ onMounted(() => {
                   </div>
                 </div>
               </template>
-
             </div>
           </div>
         </div>
