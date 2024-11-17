01d532b16291026f9409e51d7ffe7926534fac68
Eduarduar <eduardoarcega1997@gmail.com>
2024-11-17 17:08:24 -0600

    fix: actualizar las URLs base para producción

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,6 +1,7 @@
-const Baseurl = 'http://localhost:8000/'
-// const Baseurl = 'http://127.0.0.1:8001/'
-const Baseurl2 = 'ws://localhost:8000/'
-// const Baseurl2 = 'ws://localhost:8001/'
+const Baseurl = 'https://backend-summerlearn.onrender.com/'
+// const Baseurl = 'http://127.0.0.1:8001/'  // Solo para desarrollo local
+
+const Baseurl2 = 'wss://backend-summerlearn.onrender.com/'
+// const Baseurl2 = 'ws://localhost:8001/'  // Solo para desarrollo local
 
 export { Baseurl, Baseurl2 }
