8e01bac10df2b8f4257782b5552fe0dbf1984625
Eduarduar <eduardoarcega1997@gmail.com>
2024-11-17 19:02:48 -0600

    refactor: eliminar archivo .htaccess y optimizar el código del pie de página en OnePageFooter.vue

--- src/components/OnePage/OnePageFooter.vue
+++ src/components/OnePage/OnePageFooter.vue
@@ -1,33 +1,22 @@
 <template>
-  <footer
-    class="text-black dark:text-slate-200 py-10 flex flex-row justify-center items-center w-full bg-footerColor"
-  >
+  <footer class="text-black dark:text-slate-200 py-10 flex flex-row justify-center items-center w-full bg-footerColor">
     <div class="container">
       <div class="flex flex-row w-full">
         <div class="w-1/2 flex flex-col justify-center items-start">
           <!-- Display the copyright text -->
           <p class="text-left w-full">{{ $t('footer.copyRight') }}</p>
           <!-- Link to privacy notices -->
-          <a
-            href="/privacy"
-            target="_blank"
-            class="text-left w-fit text-gray-700 hover:underline"
-            >{{ $t('footer.privacyNotices') }}</a
-          >
+          <a href="/privacy" target="_blank" class="text-left w-fit text-gray-700 hover:underline">{{
+            $t('footer.privacyNotices') }}</a>
         </div>
         <div class="w-1/2 flex flex-row justify-end">
           <div class="flex flex-col gap-2">
             <!-- Label for language selection -->
-            <label for="language-select" class="w-full text-center"
-              >{{ $t('footer.language') }}:</label
-            >
+            <label for="language-select" class="w-full text-center">{{ $t('footer.language') }}:</label>
             <!-- Dropdown for language selection -->
-            <select
-              id="language-select"
+            <select id="language-select"
               class="block appearance-none bg-yellow/20 border border-yellow/30 text-black dark:text-slate-200 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-yellow/20 focus:border-yellow/300 cursor-pointer"
-              v-model="language"
-              @change="changeLanguage"
-            >
+              v-model="language" @change="changeLanguage">
               <!-- Option to activate language -->
               <option :value="`${$t('footer.values.activate.activate')}`">
                 {{ $t('footer.values.activate.lenguage') }}
@@ -46,17 +35,18 @@
 
 <script setup>
 import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
+const router = useRouter()
 
 const language = ref(route.params.lang || 'es') // Get the current language from the URL or use 'es' as default
 
 const changeLanguage = () => {
   // Check if the selected language is different from the current language
   if (language.value !== route.params.lang) {
-    // Redirect and fully reload the page
-    window.location.href = `/${language.value}`
+    // Use the router to change the language without fully reloading the page
+    router.push({ path: `/${language.value}` })
   }
 }
 </script>
